7d6bd6e797df4a5c88ec05836583dc2f5230efd3
Amir Ghafoorzade <amirghafoorzade@gmail.com>
2025-12-26 03:39:07 +0330

    fix: some ui issue

--- src/components/consumer/quizzes/client-quizzes.tsx
+++ src/components/consumer/quizzes/client-quizzes.tsx
@@ -128,7 +128,7 @@ export function ClientQuizzes({
     }
 
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4">
         {filteredQuizzes.map((quiz) => (
           <QuizCard
             key={quiz.id}
